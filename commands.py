562827739cf4cd8f31889d9552acddc9e0b89af1
Ilya Malyavskiy <malyavskiy.ilya@automiq.ru>
2016-12-24 16:59:44 +0700

    Starting to move to OOP paradigm.

---
+++ commands.py
@@ -0,0 +1,22 @@
+class CmdBase:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+class CmdCountIf(CmdBase):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+class CmdChangeIf(CmdBase):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
